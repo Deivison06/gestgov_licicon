f895aa57434fd04081d10746549b2c9100688c00
Deivison Santos <gomesdeivison060@gmail.com>
2026-06-25 23:23:50 -0300

    fix: comment out signature block for individual winner in price registration document

--- resources/views/Admin/Processos/pdf-finalizacao/pregao_eletronico/ata_registro_precos.blade.php
+++ resources/views/Admin/Processos/pdf-finalizacao/pregao_eletronico/ata_registro_precos.blade.php
@@ -878,11 +878,11 @@
         {{-- Assinatura do vencedor: a Ata é individual por empresa, então exibe somente
              o representante da empresa vinculada a esta Ata ($vencedores já vem filtrado
              para o único vencedor pelo FinalizacaoPdfService). --}}
-        <div class="signature-block">
+        {{-- <div class="signature-block">
             ___________________________________<br>
             {{ $finalizacao->responsavel ?? '' }} <br>
             {{ $finalizacao->orgao_responsavel ?? '' }}
-        </div>
+        </div> --}}
 
         {{-- Assinaturas dos vencedores --}}
         @if($vencedores && $vencedores->count() > 0)
