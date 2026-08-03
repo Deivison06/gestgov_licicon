e0a41a90fbc5510e8097bfe2a79143788b01e9f0
Deivison Santos <gomesdeivison060@gmail.com>
2026-08-03 17:39:04 -0300

    style: adjust table column widths in termo homologação for improved layout consistency

--- resources/views/Admin/Processos/pdf-finalizacao/pregao_eletronico/termo_homologacao.blade.php
+++ resources/views/Admin/Processos/pdf-finalizacao/pregao_eletronico/termo_homologacao.blade.php
@@ -198,11 +198,11 @@
                             <!-- Cabeçalho da Tabela -->
                             <tr style="background-color:#e0e0e0;">
                                 <td style="padding:6px; font-weight:bold; text-align:center; width:5%;">ITEM</td>
-                                <td style="padding:6px; font-weight:bold; text-align:center; width:50%;">DESCRIÇÃO</td>
+                                <td style="padding:6px; font-weight:bold; text-align:center; width:56%;">DESCRIÇÃO</td>
                                 <td style="padding:6px; font-weight:bold; text-align:center; width:5%;">UND.</td>
                                 <td style="padding:6px; font-weight:bold; text-align:center; width:12%;">QUANT.</td>
-                                <td style="padding:6px; font-weight:bold; text-align:center; width:12%;">VALOR UNT.</td>
-                                <td style="padding:6px; font-weight:bold; text-align:center; width:16%;">VALOR TOTAL</td>
+                                <td style="padding:6px; font-weight:bold; text-align:center; width:10%;">VALOR UNT.</td>
+                                <td style="padding:6px; font-weight:bold; text-align:center; width:12%;">VALOR TOTAL</td>
                             </tr>
 
                             <!-- Itens do Lote -->
